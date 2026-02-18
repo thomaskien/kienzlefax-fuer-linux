85f6abe13cabe14fde2e7d13808c73abe4d0391c
thomaskien <tk@mampf.net>
2026-02-18 23:23:08 +0100

    Update worker.sh

--- installer-modular/worker.sh
+++ installer-modular/worker.sh
@@ -900,6 +900,5 @@ if __name__ == "__main__":
         sys.exit(0)
 PY
 
-sudo chmod +x /usr/local/bin/kienzlefax-worker.py
-sudo systemctl restart kienzlefax-worker
-sudo systemctl status kienzlefax-worker --no-pager -l
+chmod +x /usr/local/bin/kienzlefax-worker.py
+
